1d9032e83ea256062affe3881ba3254370b54ede
xamarin <xamarin@xamarin.com>
2016-07-05 12:35:53 -0400

    [Apps] Move apps to /apps directory.

--- connected-starter-app/Yammerly/Yammerly.Service/DataObjects/StorageToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yammerly.Service.DataObjects
-{
-    public class StorageToken
-    {
-        public string Name { get; set; }
-        public System.Uri Uri { get; set; }
-        public string SasToken { get; set; }
-    }
-}
--- connected-starter-app/Yammerly/Yammerly/Models/StorageToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yammerly.Models
-{
-    public class StorageToken
-    {
-        public string Name { get; set; }
-        public System.Uri Uri { get; set; }
-        public string SasToken { get; set; }
-    }
-}
